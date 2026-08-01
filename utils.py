1c53fc03b322b924211db0149d4a536dcfab00f4
Naphtali Ishaya <naphtaliishaya640@gmail.com>
2026-08-01 04:43:28 +0100

    Initial structure for Naphtech IDS v2.0

---
+++ utils.py
@@ -0,0 +1,11 @@
+
+def banner():
+    print("=" * 60)
+    print("               NAPHTECH HUB")
+    print("      Naphtech IDS - Intrusion Detection System")
+    print("-" * 60)
+    print("Developer : Naphtali Ishaya")
+    print("Version   : 2.0.0")
+    print("Status    : Stable")
+    print("Motto     : Detect. Analyze. Defend.")
+    print("=" * 60)
